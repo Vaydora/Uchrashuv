208a2a7bae41b75ea591859e66ed0a1437b615fd
Vaydora <z.akhmadjan@gmail.com>
2025-08-26 17:50:22 +0500

    main.py

--- main.py
+++ main.py
@@ -1,17 +1,8 @@
 # main.py
 from telegram.ext import ApplicationBuilder, CommandHandler, CallbackQueryHandler, MessageHandler, filters
 from config import BOT_TOKEN
-from .handlers import (
-    start,
-    accept_rules,
-    select_date,
-    select_time,
-    handle_survey,
-    save_obraz,
-    admin_panel,
-    admin_text_input
-)
-from .keyboards import rules_keyboard, obraz_keyboard, available_dates_keyboard, time_keyboard
+from handlers import start, accept_rules, select_date, select_time, handle_survey, save_obraz, admin_panel, admin_text_input
+from keyboards import rules_keyboard, obraz_keyboard, available_dates_keyboard, time_keyboard
 
 def main():
     # Botni ishga tushirish
